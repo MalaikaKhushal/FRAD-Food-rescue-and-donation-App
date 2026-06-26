278c6fb3f6f90a990ac566b65bb8e253ad1b5826
Malaika Khushal <malaika.sp24.bse.037@gmail.com>
2026-06-26 23:41:48 +0500

    Firebase Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'screens/splashscreen.dart';
 import 'screens/landingpage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const FRADApp());
 }
 
